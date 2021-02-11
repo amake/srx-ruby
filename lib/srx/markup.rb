b343dfefa8a3ec23cccbb2647a64f5414f05c917
Aaron Madlon-Kay <aaron@madlon-kay.com>
2021-02-11 21:51:02 +0900

    Simplify XML tag patterns

--- lib/srx/markup.rb
+++ lib/srx/markup.rb
@@ -5,12 +5,12 @@ module Srx
   #
   # @see https://www.w3.org/TR/xml/
   module Markup
-    NAME_START_CHAR = /:|[A-Z]|_|[a-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|
-                       [\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|
-                       [\uF900-\uFDCF]|[\uFDF0-\uFFFD]|[\u{10000}-\u{EFFFF}]/x.freeze
-    NAME_CHAR = /#{NAME_START_CHAR}|-|\.|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]/.freeze
+    # rubocop:disable Layout/LineLength
+    NAME_START_CHAR = /[:A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u{10000}-\u{EFFFF}]/.freeze
+    # rubocop:enable Layout/LineLength
+    NAME_CHAR = /#{NAME_START_CHAR}|[-.0-9\u00B7\u0300-\u036F\u203F-\u2040]/.freeze
     NAME = /#{NAME_START_CHAR}#{NAME_CHAR}*/.freeze
-    SPACE = /(?:\u0020|\u0009|\u000D|\u000A)+/.freeze
+    SPACE = /[\u0020\u0009\u000D\u000A]+/.freeze
     EQUALS = /#{SPACE}?=#{SPACE}?/.freeze
     ENTITY_REF = /&#{NAME};/.freeze
     CHAR_REF = /&#[0-9]+;|&#x[0-9a-fA-F]+;/.freeze
